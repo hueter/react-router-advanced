65113edd0a0a6b4bc7ddbe9c96e25e19d1019b8c
Michael Hueter <mthueter@gmail.com>
2018-07-25 13:40:12 -0700

    first steps toward auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100); // fake async
+  }
+};
+
+// Different Routes
+
 const Home = props => (
   <div>
     <h1>Welcome Home</h1>
@@ -10,9 +24,28 @@ const Home = props => (
 const About = props => (
   <h1>You already know everything about me you need to know... So go away </h1>
 );
+
+const Login = props => (
+  <div>
+    <h1>Login</h1>
+  </div>
+);
+
 const NotFound = props => (
   <h1>{`${props.location.pathname} is not a valid route for this app.`}</h1>
 );
+
+// Our custom PrivateRoute component
+
+const PrivateRoute = props => {
+  if (fakeAuth.isAuthenticated) {
+    // go to props.component
+  } else {
+    // redirect back to /login
+  }
+};
+
+// Main App
 
 class App extends Component {
   render() {
@@ -25,7 +58,7 @@ class App extends Component {
           <Route exact path="/about" component={About} />
           {/* this is a 404 handler */}
           {/* <Route component={NotFound} /> */}
-
+          <Route exact path="/login" component={Login} />
           {/* Below is a redirect back home; this has to be at the bottom as a catch-all */}
           <Redirect to="/" />
         </Switch>
