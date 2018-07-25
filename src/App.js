20b9d4f1188ed07b72e656504cdf04620519e993
Michael Hueter <mthueter@gmail.com>
2018-07-25 12:00:49 -0700

    lecture progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+
+const Home = props => (
+  <div>
+    <h1> Homepage </h1>
+    <Route path="/taco" component={props => <small>theres a taco</small>} />
+  </div>
+);
+const About = props => (
+  <h1>You already know everything about me you need to know... So go away </h1>
+);
+const NotFound = props => (
+  <h1>{`${props.location.pathname} is not a valid route for this app.`}</h1>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          {/* this is a 404 handler */}
+          {/* <Route component={NotFound} /> */}
+
+          {/* Below is a redirect back home */}
+          <Redirect to="/" />
+        </Switch>
       </div>
     );
   }
---
+++ src/scratch.js
@@ -0,0 +1,12 @@
+let x = 'cat';
+
+switch (x) {
+  case 'cat':
+    console.log('x is a cat');
+    break;
+  case 'dog':
+    console.log('x is a dog');
+    break;
+  default:
+    console.log('I dont know what x is');
+}
